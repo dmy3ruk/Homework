e9e1465159c41321493f2899e795311f7315992f
dmy3ruk <104530429+dmy3ruk@users.noreply.github.com>
2022-11-13 20:36:42 +0200

    Add files via upload

---
+++ const numbers = [1, 2, 3, 4, 5, 6].js
@@ -0,0 +1,8 @@
+    const numbers = [1, 2, 3, 4, 5, 6];
+    const text = ["one", "two", "three", "four", "five", "six"];
+    const mas=[] 
+ function convert(nmb){ 
+            return  text[nmb-1]
+}for (let i=0; i<numbers.length; i++)
+mas[i]=convert(numbers[i])
+console.log(mas)
